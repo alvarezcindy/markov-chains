4771d87b923c04da16ea83df10594347143275fe
Hackbright Student <no-reply@hackbrightacademy.com>
2018-06-26 17:42:58 -0700

    cleaned up code and allowed make_chains function to take 'n' number of words in key

--- markov.py
+++ markov.py
@@ -2,6 +2,8 @@
 
 from random import choice
 import random
+import sys
+
 
 
 def open_and_read_file(file_path):
@@ -14,11 +16,12 @@ def open_and_read_file(file_path):
     return read_file
 
 
-def make_chains(text_string):
+def make_chains(text_string, n=2):
     """Take input text as string; return dictionary of Markov chains.
 
-    A chain will be a key that consists of a tuple of (word1, word2)
-    and the value would be a list of the word(s) that follow those two
+    A chain will be a key that consists of a tuple of "n" length of words 
+    (word1, word2 ...wordn)
+    and the value would be a list of the word(s) that follow those 
     words in the input text.
 
     For example:
@@ -29,51 +32,54 @@ def make_chains(text_string):
 
         >>> sorted(chains.keys())
         [('hi', 'there'), ('mary', 'hi'), ('there', 'mary')]
-
     Each item in chains is a list of all possible following words:
 
         >>> chains[('hi', 'there')]
         ['mary', 'juanita']
-        
+      
         >>> chains[('there','juanita')]
         [None]
     """
 
     chains = {}
 
-    words = text_string.split()
+    words = tuple(text_string.split())
 
-    for i in range(len(words)-2):
-        pair = (words[i], words[i + 1])
-        if pair in chains:
-            pair_value = chains[pair]
-            pair_value.append(words[i + 2])
-        else:
-            chains[pair] = [words[i+2]]
+    for i in range(len(words)-n):
+        pair = words[i:i+n]
+
+        if pair not in chains:
+            chains[pair] = []
+
+        chains[pair].append(words[i+n])
+   
     return chains
 
 
 def make_text(chains):
     """Return text from chains."""
 
-    words = []
-
     key = random.choice(list(chains.keys()))
+    words = list(key)
 
     while True:
-        if key in chains:
-            new = random.choice(chains[key])
-            words.extend([key[0], key[1], new])
-            key = (key[1], new)
-        else:
+        if key not in chains:
             break
+
+        new = random.choice(chains[key])
+        words.append(new)
+        key = key[1:] + (new,)
+
     return " ".join(words)
 
 
-input_path = "green-eggs.txt"
+# filename = sys.argv[0]
+text = sys.argv[1]
+
+# input_path = "green-eggs.txt"
 
 # Open the file and turn it into one long string
-input_text = open_and_read_file(input_path)
+input_text = open_and_read_file(text)
 
 # Get a Markov chain
 chains = make_chains(input_text)
